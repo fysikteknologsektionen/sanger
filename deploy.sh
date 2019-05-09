c1f234b47ae47696acd810fed81b0a2a2917c213
JohanWinther <johan.winther@outlook.com>
2019-05-09 15:01:08 +0200

    Script klar

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+# Sånghäfte
 cd ../Sanghafte/texter/
 rm -f *
 cp ../../Sangtexter/* .
 rm deploy.sh
 rm install.sh
-git add *
+git add .
 git commit -m "Auto-deployed database"
 git push origin master
 git reset --hard overleaf/master
@@ -12,7 +13,26 @@ rm -f *
 cp ../../Sangtexter/* .
 rm deploy.sh
 rm install.sh
-git add *
+git add .
+git commit -m "Auto-deployed database"
+git push overleaf master
+git reset --hard origin/master
+
+# Sjungbok
+cd ../../Sjungbok/texter/
+rm -f *
+cp ../../Sangtexter/* .
+rm deploy.sh
+rm install.sh
+git add .
+git commit -m "Auto-deployed database"
+git push origin master
+git reset --hard overleaf/master
+rm -f *
+cp ../../Sangtexter/* .
+rm deploy.sh
+rm install.sh
+git add .
 git commit -m "Auto-deployed database"
 git push overleaf master
 git reset --hard origin/master
