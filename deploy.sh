7de2857be15d5210b4a6d5bd64385a606c4c8af5
David Hambraeus <ddhambraeus@gmail.com>
2019-05-09 22:02:49 +0200

    deploy deployar nu bara till github och ändringar bör pullas via overleaf

--- deploy.sh
+++ deploy.sh
@@ -10,17 +10,6 @@ rm README.md
 git add .
 git commit -m "Deployed database $(date +%Y-%m-%d)"
 git push origin master
-git fetch overleaf
-git reset --hard overleaf/master
-git pull overleaf master
-rm -f *
-cp ../../Sangtexter/* .
-rm deploy.sh
-rm install.sh
-git add .
-git commit -m "Deployed database $(date +%Y-%m-%d)"
-git push overleaf master
-git reset --hard origin/master
 
 # Sjungbok
 cd ../../Sjungbok/texter/
@@ -32,14 +21,4 @@ rm README.md
 git add .
 git commit -m "Auto-deployed database"
 git push origin master
-git fetch overleaf
-git reset --hard overleaf/master
-git pull overleaf master
-rm -f *
-cp ../../Sangtexter/* .
-rm deploy.sh
-rm install.sh
-git add .
-git commit -m "Auto-deployed database"
-git push overleaf master
-git reset --hard origin/master
+
