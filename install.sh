9ed6173f32723102f4f577f03ebe25c1d056af2d
Vesterlund <13664409+Vesterlund@users.noreply.github.com>
2022-05-17 16:42:17 +0200

    Uppdatera install.sh till main branch

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ cd ..
 git clone https://github.com/Fysikteknologsektionen/Sanghafte.git
 git clone https://github.com/Fysikteknologsektionen/Sjungbok.git
 cd Sanghafte
-git pull origin master
+git pull origin main
 cd ../Sjungbok
-git pull origin master
+git pull origin main
